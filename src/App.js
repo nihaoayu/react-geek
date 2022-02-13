4c20abbf657f84c8149349a28ea5fe666e4dee7a
啊宇 <2961062796@qq.com>
2022-02-13 21:33:04 +0800

    根组件配置路由

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import './App.scss'
-
+import Layout from './pages/layout'
+import Login from './pages/login'
+import NotFound from './pages/404'
 function App () {
   return (
-    <div className="App">
-      app
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Redirect exact from='/' to='/home' />
+          <Route path='/home' component={Layout} />
+          <Route path='/login' component={Login} />
+          {/* 404页面 */}
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   )
 }
 
