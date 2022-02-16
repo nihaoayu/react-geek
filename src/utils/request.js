4b8ae78c2574935e8c6e2fc767db67db37979032
啊宇 <2961062796@qq.com>
2022-02-16 19:35:18 +0800

    注入全局请求头

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,18 @@
 import axios from "axios"
-
+import store from "@/store"
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0'
 })
+// 请求拦截器
+request.interceptors.request.use(config => {
+  const { token } = store.getState()
 
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+// 响应拦截器
 request.interceptors.response.use(response => {
   return response.data
 })
