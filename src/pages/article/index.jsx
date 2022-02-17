14db166f19bde066daf5abd3405d4b3fa4588f4f
啊宇 <2961062796@qq.com>
2022-02-17 21:38:50 +0800

    内容管理页面表格渲染

--- src/pages/article/index.jsx
+++ src/pages/article/index.jsx
@@ -1,14 +1,92 @@
 import { Link } from 'react-router-dom'
-import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select } from 'antd'
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Radio,
+  DatePicker,
+  Select,
+  Table,
+  Tag,
+  Space,
+} from 'antd'
 import 'moment/locale/zh-cn'
 import locale from 'antd/es/date-picker/locale/zh_CN'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+
+import img404 from '@/assets/error.png'
 
 const { Option } = Select
 const { RangePicker } = DatePicker
 
 const Article = () => {
+  const columns = [
+    {
+      title: '封面',
+      dataIndex: 'cover',
+      render: (cover) => {
+        return <img src={cover || img404} width={200} height={150} alt="" />
+      },
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      width: 220,
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      render: (data) => <Tag color="green">审核通过</Tag>,
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'pubdate',
+    },
+    {
+      title: '阅读数',
+      dataIndex: 'read_count',
+    },
+    {
+      title: '评论数',
+      dataIndex: 'comment_count',
+    },
+    {
+      title: '点赞数',
+      dataIndex: 'like_count',
+    },
+    {
+      title: '操作',
+      render: (data) => {
+        return (
+          <Space size="middle">
+            <Button type="primary" shape="circle" icon={<EditOutlined />} />
+            <Button
+              type="primary"
+              danger
+              shape="circle"
+              icon={<DeleteOutlined />}
+            />
+          </Space>
+        )
+      },
+    },
+  ]
+
+  const data = [
+    {
+      id: '8218',
+      comment_count: 0,
+      cover: 'http://geek.itheima.net/resources/images/15.jpg',
+      like_count: 0,
+      pubdate: '2019-03-11 09:00:00',
+      read_count: 2,
+      status: 2,
+      title: 'webview离线化加载h5资源解决方案',
+    },
+  ]
   return (
-    <div>
+    <>
       <Card
         title={
           <Breadcrumb separator=">">
@@ -51,7 +129,10 @@ const Article = () => {
           </Form.Item>
         </Form>
       </Card>
-    </div>
+      <Card>
+        <Table columns={columns} dataSource={data} rowKey="id" />
+      </Card>
+    </>
   )
 }
 
