9dcb6ed99da08500deec4ab00c19c2e9ad0d3299
啊宇 <2961062796@qq.com>
2022-02-17 21:29:55 +0800

    内容管理页面基础渲染

--- src/pages/article/index.jsx
+++ src/pages/article/index.jsx
@@ -1,7 +1,56 @@
-function Article() {
+import { Link } from 'react-router-dom'
+import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select } from 'antd'
+import 'moment/locale/zh-cn'
+import locale from 'antd/es/date-picker/locale/zh_CN'
+
+const { Option } = Select
+const { RangePicker } = DatePicker
+
+const Article = () => {
   return (
     <div>
-      <h1>Article</h1>
+      <Card
+        title={
+          <Breadcrumb separator=">">
+            <Breadcrumb.Item>
+              <Link to="/home">首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>内容管理</Breadcrumb.Item>
+          </Breadcrumb>
+        }
+        style={{ marginBottom: 20 }}>
+        <Form initialValues={{ status: -1 }}>
+          <Form.Item label="状态" name="status">
+            <Radio.Group>
+              <Radio value={-1}>全部</Radio>
+              <Radio value={0}>草稿</Radio>
+              <Radio value={1}>待审核</Radio>
+              <Radio value={2}>审核通过</Radio>
+              <Radio value={3}>审核失败</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label="频道" name="channel_id">
+            <Select
+              placeholder="请选择文章频道"
+              // defaultValue="lucy"
+              style={{ width: 120 }}>
+              <Option value="jack">Jack</Option>
+              <Option value="lucy">Lucy</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="日期" name="date">
+            <RangePicker locale={locale}></RangePicker>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              筛选
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   )
 }
